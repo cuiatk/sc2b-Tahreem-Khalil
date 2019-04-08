18f1a57a1c0550380bce73c738d95a5ec99f1a99
Tahreem-Khalil <tahreem.khalil173@gmail.com>
2019-04-09 02:02:57 +0500

    changes in testcases and testing strategy

--- tweets/test/twitter/ExtractTest.java
+++ tweets/test/twitter/ExtractTest.java
@@ -22,15 +22,15 @@ public class ExtractTest {
      * 
      * testing strategy for getTimespan and getMentionedUsers
      * 
-     * partition space for getTimeSpan
-     * check if the list is empty if not empty then,
-     * check if instant d1 is the starting point
-     * check if instant d3 is the ending point
+     * partition space for getTimespan(tweets) -> timespan
+     * tweet.size(): 1,>1
+     * check tweets having similar timspan
+     * check if instant d1, d3 is the starting and ending instants
      * 
-     * partition for getMentionedUsers
-     * check if tweet list is empty if not empty then,
-     * check if tweets are null
-     * check if tweet has mention once
+     * partition for getMentionedUsers(tweets) -> mentionedUsers
+     * tweet.size(): 0,1,>1
+     * mentions in tweet: 0,1,>1
+     * user names with mixed cases: upper, lower, mixed-case
      * check if any tweet has mentions twice
      * 
      * See the ic03-testing exercise for examples of what a testing strategy comment looks like.
@@ -44,6 +44,9 @@ public class ExtractTest {
     private static final Tweet tweet1 = new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", d1);
     private static final Tweet tweet2 = new Tweet(2, "bbitdiddle", "@rivest talk in 30 minutes #hype", d2);
     private static final Tweet tweet3 = new Tweet(3, "yassGirl", "to be or not to be. @byeBoss @yoloGirl", d3);
+    private static final Tweet tweet4 = new Tweet(4, "tahreem_k", "you should see me in a crown.", d2);
+    private static final Tweet tweet5 = new Tweet(5, "starry_girl", "desi parents are too much.", d2);
+    private static final Tweet tweet6 = new Tweet(6, "likeWise_b", "@hobbit_man @BLUE_NOT @CANyou_not get ready.", d2);
     
     
     
@@ -52,48 +55,75 @@ public class ExtractTest {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
-    /*
-     * checking if list is empty, and also asserting instants time
-     */
     
     @Test
-    public void testGetTimespanTwoTweets() {
+    /*
+     * test case: checking timespan one tweet
+     * tweet.size = 1
+     * 
+     */
+    public void testGetTimespanOfOneTweet() {
+        Timespan timespan = Extract.getTimespan(Arrays.asList(tweet1));
+        
+        assertEquals("expected start", d1, timespan.getStart());
+        assertEquals("expected end", d1, timespan.getEnd());
+        
+        assertFalse("expected empty tweets list",Arrays.asList(timespan).isEmpty());
+    }
+    
+    
+    @Test
+    /*
+     * test case: checking timespan two tweet
+     * tweet.size >1
+     * 
+     */
+    public void testGetTimespanOftwoTweet() {
+        Timespan timespan = Extract.getTimespan(Arrays.asList(tweet1,tweet2));
+        
+        assertEquals("expected start", d1, timespan.getStart());
+        assertEquals("expected end", d2, timespan.getEnd());
+        
+        assertFalse("expected empty tweets list",Arrays.asList(timespan).isEmpty());
+    }
+    
+    @Test
+    /*
+     * test case: checking timespan two tweet
+     * tweet.size >1
+     * 
+     */
+    public void testGetTimespanOfMultipleTweet() {
         Timespan timespan = Extract.getTimespan(Arrays.asList(tweet1,tweet2,tweet3));
         
         assertEquals("expected start", d1, timespan.getStart());
         assertEquals("expected end", d3, timespan.getEnd());
         
-        assertFalse("not expected empty tweets list",Arrays.asList(timespan).isEmpty());
-       
-        assertEquals("tweet 1 id is null", tweet1.getId(), tweet1.getId());
-        assertEquals("tweet 2 id is null", tweet2.getId(), tweet2.getId());
-        
-
-       // assertEquals("instant d1 is not equal ", d1, timespan.getStart().is);
-        //assertEquals("instant d2 is not equal ", d2, timespan.getStart().toString()=="");
-        assertNotNull("instance 2 is null", d2);
+        assertFalse("expected empty tweets list",Arrays.asList(timespan).isEmpty());
     }
     
-    /*
-     * checking if list is empty
-     */
     
+    /*
+     * test case: check if tweet list is empty
+     * tweet.size = 0
+     * mentions = 0
+     */
     @Test
-    public void testGetMentionedUsersNoMention() {
-        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1,tweet2,tweet3));
+    public void testGetMentionedUsersNoMentionNoTweet() {
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList());
         
-        assertFalse("expected empty set", mentionedUsers.isEmpty());
-        
-        assertNotNull("tweet 1 is null", tweet1);
-        assertNotNull("tweet 2 is null", tweet2);
+        assertTrue("expected empty set", mentionedUsers.isEmpty());
+      
         
     }
     
     /*
-     * checking if tweet has one mention
+     * test case: one tweet one mention
+     * tweet.size = 1
+     * mention = 1
      */
     @Test
-    public void testGetMentionedUsersOneMentionTweet() {         
+    public void testGetMentionedUsersOneMentionOneTweet() {         
         Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet2));
         Set<String> mentionedUsersLowerCase = new HashSet<>();
         for (String mentionedUser : mentionedUsers) {
@@ -103,18 +133,86 @@ public class ExtractTest {
     }
     
     /*
-     * checking if tweet has two mentions
+     * test case: one tweet zero mention
+     * tweet.size = 1
+     * mention = 0
      */
     @Test
-    public void testGetMentionedUsersTwoMentionTweet() {         
+    public void testGetMentionedUsersZeroMentionOneTweet() {         
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1));
+       assertEquals("expected no mentions",0,mentionedUsers.size());
+    }
+    
+    /*
+     * checking one tweet has two mentions
+     * tweet.size = 1
+     * mentions = >1
+     */
+    @Test
+    public void testGetMentionedUsersTwoMentionOneTweet() {         
         Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet3));
         Set<String> mentionedUsersLowerCase = new HashSet<>();
         for (String mentionedUser : mentionedUsers) {
             mentionedUsersLowerCase.add(mentionedUser.toLowerCase());
         }
         assertTrue(mentionedUsersLowerCase.containsAll(Arrays.asList("byeboss", "yologirl")));
-    
     }
+    
+    /*
+     * checking multiple tweet has one mentions
+     * tweet.size = >1
+     * mentions = 1
+     */
+    @Test
+    public void testGetMentionedUsersOneMentionMultipleTweet() {         
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1, tweet2));
+        Set<String> mentionedUsersLowerCase = new HashSet<>();
+        for (String mentionedUser : mentionedUsers) {
+            mentionedUsersLowerCase.add(mentionedUser.toLowerCase());
+        }
+        assertTrue(mentionedUsersLowerCase.containsAll(Arrays.asList("rivest")));
+    }
+    
+    /*
+     * checking multiple tweet has multiple mentions
+     * tweet.size = >1
+     * mentions = >1
+     */
+    @Test
+    public void testGetMentionedUsersMultipleMentionMultipleTweet() {         
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1, tweet2, tweet3));
+        Set<String> mentionedUsersLowerCase = new HashSet<>();
+        for (String mentionedUser : mentionedUsers) {
+            mentionedUsersLowerCase.add(mentionedUser.toLowerCase());
+        }
+        assertTrue(mentionedUsersLowerCase.containsAll(Arrays.asList("rivest","yologirl","byeboss")));
+    }
+    
+    /*
+     * checking multiple tweet has zero mentions
+     * tweet.size = >1
+     * mentions = 0
+     */
+    @Test
+    public void testGetMentionedUserZeroMentionMultipleTweet() {         
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1, tweet4, tweet5));
+        
+        assertEquals("expected no mentions",0, mentionedUsers.size());
+    }
+    
+    /*
+     * checking multiple tweet has multiple mentions lower case
+     * tweet.size = >1
+     * mentions = >1
+     * case : lower, upper, mixed case
+     */
+    @Test
+    public void testGetMentionedUserMultipleMentionMixedCase() {         
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet6));
+        
+        assertTrue("expected to have all mentions in lower case",mentionedUsers.containsAll(Arrays.asList("hobbit_man","blue_not","canyou_not")));
+    }
+    
     /*
      * Warning: all the tests you write here must be runnable against any
      * Extract class that follows the spec. It will be run against several staff
